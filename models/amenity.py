b92095e0b8c564d83e1603b150bcd344a2af2d81
shane <6874@holbertonstudents.com>
2023-12-01 14:00:06 +0100

    working on task 7 witho Solomon

--- models/amenity.py
+++ models/amenity.py
@@ -2,15 +2,13 @@
 """ Amenity module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from os import getenv
-
+from models.place import place_amenity
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     """ Amenity class to store amenity information """
 
     __tablename__ = 'amenities'
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
--- models/review.py
+++ models/review.py
@@ -2,7 +2,8 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from os import getenv
+
+
 
 
 class Review(BaseModel, Base):
@@ -10,11 +11,7 @@ class Review(BaseModel, Base):
 
     __tablename__ = 'reviews'
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
