65063992fc899c39431850a1a9c22e03c5036eb4
shane <6874@holbertonstudents.com>
2023-12-13 10:07:14 +0100

    correcting pycode task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,16 +7,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Display 'Hello HBNB!'"""
     message = "Hello HBNB!"
     return message
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """display 'hbnb'"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
