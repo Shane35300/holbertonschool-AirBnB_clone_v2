255840474f899b18882cad28859a6eb22d46b503
shane <6874@holbertonstudents.com>
2023-12-14 14:06:01 +0100

    task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -4,14 +4,35 @@ Start a flask web application
 """
 
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session after each request."""
+    storage.close()
+
+
 @app.route('/hbnb_filter', strict_slashes=False)
-def display_html():
+def hbnb_filters():
     """display a HTML page like 6-index.html'"""
-    return render_template('10-hbnb_filters.html')
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    sorted_amenities = sorted(amenities, key=lambda x: x.name)
+
+    # Triez les villes de chaque État
+    for state in sorted_states:
+        state.cities = sorted(state.cities, key=lambda x: x.name)
+
+    return render_template('10-hbnb_filters.html',
+                           states=sorted_states,
+                           amenities=sorted_amenities)
+
 
 
 if __name__ == '__main__':
